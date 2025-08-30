236d0adeb5200d95d811ee403a4829ce51f0aede
Rahul32-code <114091763+Rahul32-code@users.noreply.github.com>
2025-08-30 23:46:16 +0530

    url add

--- src/components/MyServiceButton.jsx
+++ src/components/MyServiceButton.jsx
@@ -5,7 +5,7 @@ const MyServiceButton = () => {
   return (
     <div className="fixed top-25 right-5 z-30 flex items-center justify-center animate-fade-in duration-500  animate-pulse">
       <a
-        href=""
+        href="https://rahul-service.vercel.app/"
         title="Rahul Service"
         className="p-2 border-2 border-teal-400 rounded-lg shadow-lg transition-all duration-300 transform hover:scale-110 "
       >
